1204230a438aa9b5204c8737e6d6205fdd191c2d
hashini0123 <nanayakkarahashini5@gmail.com>
2026-01-21 11:46:49 +0530

    Initial Commit - Student class added

---
+++ src/main/java/edu/icet/Main.java
@@ -0,0 +1,32 @@
+package edu.icet;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.setId(101);
+        student.setName("amal");
+        student.setAge(20);
+
+        Configuration config = new Configuration();
+        SessionFactory factory = config.buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        session.persist(student);
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/edu/icet/Student.java
@@ -0,0 +1,34 @@
+package edu.icet;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
+
